e05dfcb4d91cde90bd5b829d8d60d09a25b3cb1b
Josh Batchelor <josh_batch@live.com.au>
2014-07-22 15:08:02 +0800

    Fixed project dependencies

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
 (defproject trellolib "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A clojure library for interacting with Trello"
+  :url "http://https://github.com/jbatch/trellolib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-oauth "1.5.1"]
                  [clj-http "0.9.2"]
                  [cheshire "5.3.1"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]]}})
