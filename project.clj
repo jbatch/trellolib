ab23d122e39fcfed472a590d338781919dd28779
Josh Batchelor <josh_batch@live.com.au>
2014-07-23 14:39:53 +0800

    Rewrote a large chunk to use OAuth

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-http "0.9.2"]
-                 [cheshire "5.3.1"]]
+                 [cheshire "5.3.1"]
+                 [clj-oauth "1.5.1"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]]}})
--- src/trellolib/core.clj
+++ src/trellolib/core.clj
@@ -2,12 +2,8 @@
   (:require [trellolib.validation :refer :all]
             [cheshire.core :as cheshire]
             [clj-http.client :as httpclient]
-            [clojure.string :as str]))
-
-(def test-card {:name "hello" :desc "world" :idList "<yourid>"})
-(def test-client {:key "<yourkey>"
-                  :token "<yourtoken>"
-                  :name "<yourapplicationname>"})
+            [clojure.string :as str]
+            [oauth.client :as oauth]))
 
 (def trello-base-url "https://trello.com/1/")
 
@@ -25,11 +21,69 @@
               [(encode-param [k v])])))
          (str/join "&"))))
 
+(defn credentials-to-string
+  "Turns a set of OAuth credentials into a string for the authorization header"
+  [credentials]
+  (letfn [(encode-param [[k v]] (str (name k) "=" "\"" v "\""))]
+    (->> credentials
+         (mapcat
+          (fn [[k v]]
+            (if (or (seq? v) (sequential? v))
+              (map #(encode-param [k %]) v)
+              [(encode-param [k v])])))
+         (str/join ","))))
+
+(defn authorize-client
+  "Takes a client with a key and secret and returns the client with a
+  request-token and autorize-url attached"
+  [client]
+  (let [consumer (oauth/make-consumer (:key client)
+                                      (:secret client)
+                                      (str trello-base-url
+                                           "OAuthGetRequestToken")
+                                      (str trello-base-url
+                                           "OAuthGetAccessToken")
+                                      (str trello-base-url
+                                           "OAuthAuthorizeToken")
+                                      :hmac-sha1)
+        request-token (oauth/request-token consumer (:callback client))
+        authorize-url (str (oauth/user-approval-uri consumer
+                                                    (:oauth_token request-token))
+                           "&name=MyApp&expiration=never&scope=read,write")]
+    (-> client
+        (assoc :consumer consumer)
+        (assoc :request-token request-token)
+        (assoc :authorize-url authorize-url))))
+
+(authorize-client test-client)
+
+(defn get-access-token
+  "Takes a client and a verifier and associates the client with an
+  access token response "
+  [client verifier]
+  (let [access-token-response (oauth/access-token (:consumer client)
+                                                  (:request-token client)
+                                                  verifier)]
+    (assoc client :access-token access-token-response)))
+
+(get-access-token (authorize-client test-client) "2185eacd2ccfebe76bfeb0a6dae672f7")
+
+(defn get-credentials
+  "Takes a client and associates it with OAuth credentials for a given request"
+  [client request-type uri]
+  (let [credentials (oauth/credentials (:consumer client)
+                                       (:oauth_token (:access-token client))
+                                       (:oauth_token_secret (:access-token client))
+                                       request-type
+                                       uri)]
+    (assoc client :credentials credentials)))
+
+
 (def default-authorize-opts {:expiration "never"
                              :response_type "token"
                              :scope "read,write"})
 
-(defn authorize-url
+(defn authorize-url-old
   "Generates the url to authorize the application to access users
   Trello."
   [client]
@@ -37,14 +91,13 @@
                                     :response_type :scope])
         client (merge default-authorize-opts client)]
     (str trello-base-url
-        "authorize/?" (encode-param client))))
+         "authorize/?" (encode-param client))))
 
-(defn get-access
-  "Gets access token from user"
-  [client]
-  (println "Go to this address and copy/paste the token here")
-  (println (authorize-url client))
-  (assoc client :token (read-line)))
+(defn wrap-credentials
+  "Wraps crediantials in an Authorization header"
+  [credentials]
+  {:Authorization (str "OAuth realm=\"http://trello.com\","
+                       (credentials-to-string credentials))})
 
 (defn get-json
   "Takes an http response and returns the body as a clojure datatype"
@@ -54,11 +107,9 @@
 (defn get-url
   "Generates the url requires given the RESTful extension (ie
   \"cards\")"
-  [client req]
+  [req]
   (str trello-base-url
-       req
-       "?"
-       (encode-param (select-keys client [:key :token]))))
+       req))
 
 (defn get-lists
   "Requests a sequence of the lists on a board"
@@ -67,14 +118,19 @@
 
 (defn trello-post
   "Performs a POST request on a trello uri"
-  [body uri]
-  (httpclient/post uri {:content-type :json
+  [body client uri]
+  (httpclient/post uri {:headers (wrap-credentials (:credentials client))
+                        :content-type :json
                         :body (cheshire/encode body)}))
 
 (defn trello-get
   "Performs a GET request on a trello uri"
-  [uri]
-  (cheshire/decode (:body (httpclient/get uri))))
+  [uri client]
+  (cheshire/decode (:body
+                    (httpclient/get
+                     uri
+                     {:headers
+                      (wrap-credentials (:credentials client))}))))
 
 (defn post-card
   "Posts a new card on a list"
@@ -84,6 +140,21 @@
       (validate-card-keys)
       (validate-string-length :name)
       (validate-string-length :desc)
-      (trello-post (get-url client "cards"))
+      (trello-post (get-url client "cards") client)
       (:body)
       (cheshire/decode true)))
+
+(def card {:name "testcard" :idList "listId" :desc "card"})
+
+(def test-client {:key "yourkey"
+                  :secret "secret"
+                  :name "OAuth App"
+                  :callback "localhost"})
+
+(def new-client (get-credentials (get-access-token (authorize-client test-client) "verifier") :GET (get-url "boards/Ao0XENXN")))
+
+(wrap-credentials (:credentials new-client))
+
+(trello-get (get-url "boards/Ao0XENXN") new-client)
+(trello-get (get-url "boards/Ao0XENXN/lists") new-client)
+(trello-post card new-client (get-url "cards"))
