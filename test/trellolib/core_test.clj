a759a23236b9c27b3bc4a9ab2142fd890e6de4b3
Josh Batchelor <josh_batch@live.com.au>
2014-07-24 13:53:17 +0800

    Updated test-suite to reflect the new oauth functions

--- test/trellolib/core_test.clj
+++ test/trellolib/core_test.clj
@@ -1,7 +1,8 @@
 (ns trellolib.core-test
   (:require [midje.sweet :refer :all]
             [trellolib.core :as core]
-            [trellolib.validation :refer :all]))
+            [trellolib.validation :refer :all]
+            [oauth.client :as oauth]))
 
 
 (def test-card {:name "hello" :desc "world" :idList "<youridhere>"})
@@ -10,12 +11,46 @@
                             :idList "<youridhere>"
                             :errors '({:a "old"})})
 (def test-client {:key "<yourkey>"
-                  :token "<yourtoken>"})
+                  :secret "<yoursecret>"
+                  :callback "<yourcallback>"})
+
+(def auth-client {:consumer {:key "<yourkey>"
+                             :secret "<yoursecret>"
+                             :request-uri "https://trello.com/1/OAuthGetRequestToken"
+                             :access-uri "https://trello.com/1/OAuthGetAccessToken"
+                             :authorize-uri "https://trello.com/1/OAuthAuthorizeToken"
+                             :signature-method :hmac-sha1}
+                  :key "<yourkey>"
+                  :authorize-url "https://trello.com/1/OAuthAuthorizeToken?oauth_token=%3Ctemptoken%3E&name=MyApp&expiration=never&scope=read,write"
+                  :secret "<yoursecret>"
+                  :request-token
+                  {:oauth_token "<temptoken>"
+                   :oauth_token_secret "<tempsecret>"
+                   :oauth_callback_confirmed "true"}
+                  :callback "<yourcallback>"})
+
+(def access-client {:consumer
+                    {:key "<yourkey>"
+                     :secret "<yoursecret>"
+                     :request-uri "https://trello.com/1/OAuthGetRequestToken"
+                     :access-uri "https://trello.com/1/OAuthGetAccessToken"
+                     :authorize-uri "https://trello.com/1/OAuthAuthorizeToken"
+                     :signature-method :hmac-sha1}
+                    :key "<yourkey>"
+                    :authorize-url "https://trello.com/1/OAuthAuthorizeToken?oauth_token=%3Ctemptoken%3E&name=MyApp&expiration=never&scope=read,write"
+                    :secret "<yoursecret>"
+                    :request-token
+                    {:oauth_token "<temptoken>"
+                     :oauth_token_secret "<tempsecret>"
+                     :oauth_callback_confirmed "true"}
+                    :callback "<yourcallback>"
+                    :access-token {:oauth_token "<accesstoken>"
+                                   :oauth_token_secret "<accesssecret>"}})
 
 (facts "Core Tests"
        (fact "key-exists confirms key exists in hashmap"
              (key-exists test-card test-client :key) => test-card
-             (key-exists test-card test-client :token) => test-card
+             (key-exists test-card test-client :secret) => test-card
              (key-exists test-card test-client :not-key)
              => (contains {:errors '({:desc "Expected key not found"
                                       :field ":not-key"})}))
@@ -27,18 +62,18 @@
              (add-error
               test-card-with-errors
               {:a "new"}) => (contains {:errors '({:a "new"} {:a "old"})}))
-       ;(fact "validate-client-keys detects errors in clients"
-       ;      (validate-client-keys test-card test-client) => test-card
-       ;      (validate-client-keys test-card {:key "1234"
-       ;                                            :not-token "4321"})
-       ;      => (contains {:errors '({:desc "Expected key not found"
-       ;                               :field ":token"})})
-       ;      (validate-client-keys
-       ;       test-card
-       ;       {:not-key "1234" :not-token "4321"})
-       ;      => (contains {:errors
-       ;                    '({:desc "Expected key not found" :field ":token"}
-       ;                      {:desc "Expected key not found" :field ":key"})}))
+                                        ;(fact "validate-client-keys detects errors in clients"
+                                        ;      (validate-client-keys test-card test-client) => test-card
+                                        ;      (validate-client-keys test-card {:key "1234"
+                                        ;                                            :not-token "4321"})
+                                        ;      => (contains {:errors '({:desc "Expected key not found"
+                                        ;                               :field ":token"})})
+                                        ;      (validate-client-keys
+                                        ;       test-card
+                                        ;       {:not-key "1234" :not-token "4321"})
+                                        ;      => (contains {:errors
+                                        ;                    '({:desc "Expected key not found" :field ":token"}
+                                        ;                      {:desc "Expected key not found" :field ":key"})}))
        (fact "validate-card-keys detects errors in cards"
              (validate-card-keys test-card) => test-card
              (validate-card-keys {:name "a" :desc "b" :not-idList "c"})
@@ -63,4 +98,74 @@
               {:name (apply str (take 16385 (repeat "a")))} :name)
              => (contains {:errors '({:desc
                                       "String length not in the range 1-16384"
-                                      :field ":name"})})))
+                                      :field ":name"})}))
+       (against-background [(oauth/request-token anything anything)
+                            => {:oauth_token "<temptoken>"
+                                :oauth_token_secret "<tempsecret>"
+                                :oauth_callback_confirmed "true"}]
+                           (fact "authorize-client will return the
+                                  client passed in with a consumer,
+                                  request-token and authorize-url"
+                                 (core/authorize-client test-client)
+                                 => (just {:consumer
+                                           {:key "<yourkey>"
+                                            :secret "<yoursecret>"
+                                            :request-uri "https://trello.com/1/OAuthGetRequestToken"
+                                            :access-uri "https://trello.com/1/OAuthGetAccessToken"
+                                            :authorize-uri "https://trello.com/1/OAuthAuthorizeToken"
+                                            :signature-method :hmac-sha1}
+                                           :key "<yourkey>"
+                                           :authorize-url "https://trello.com/1/OAuthAuthorizeToken?oauth_token=%3Ctemptoken%3E&name=MyApp&expiration=never&scope=read,write"
+                                           :secret "<yoursecret>"
+                                           :request-token
+                                           {:oauth_token "<temptoken>"
+                                            :oauth_token_secret "<tempsecret>"
+                                            :oauth_callback_confirmed "true"}
+                                           :callback "<yourcallback>"})))
+
+       (against-background [(oauth/access-token anything anything "<yourverifier>")
+                            => {:oauth_token "<accesstoken>"
+                                :oauth_token_secret "<accesssecret>"}]
+                           (fact "get-access-token will return the
+                                  client with an access-token"
+                                 (core/get-access-token auth-client "<yourverifier>")
+                                 => {:consumer
+                                     {:key "<yourkey>"
+                                      :secret "<yoursecret>"
+                                      :request-uri "https://trello.com/1/OAuthGetRequestToken"
+                                      :access-uri "https://trello.com/1/OAuthGetAccessToken"
+                                      :authorize-uri "https://trello.com/1/OAuthAuthorizeToken"
+                                      :signature-method :hmac-sha1}
+                                     :key "<yourkey>"
+                                     :authorize-url "https://trello.com/1/OAuthAuthorizeToken?oauth_token=%3Ctemptoken%3E&name=MyApp&expiration=never&scope=read,write"
+                                     :secret "<yoursecret>"
+                                     :request-token
+                                     {:oauth_token "<temptoken>"
+                                      :oauth_token_secret "<tempsecret>"
+                                      :oauth_callback_confirmed "true"}
+                                     :callback "<yourcallback>"
+                                     :access-token {:oauth_token "<accesstoken>"
+                                                    :oauth_token_secret "<accesssecret>"}}))
+
+       (against-background [(oauth/credentials anything anything anything anything anything)
+                            => {:oauth_signature "<signature>"
+                                :oauth_token "<accesstoken>"
+                                :oauth_consumer_key "<yourkey>"
+                                :oauth_signature_method "HMAC-SHA1"
+                                :oauth_timestamp "<timestamp>"
+                                :oauth_nonce "<nonce>"
+                                :oauth_version "1.0"}]
+                           (fact "get-credentials will return the
+                                  clinet with credentials attached"
+                                 (core/get-credentials
+                                  access-client
+                                  :PUT
+                                  (core/get-url "cards"))
+                                 => (contains {:credentials
+                                               {:oauth_signature "<signature>"
+                                                :oauth_token "<accesstoken>"
+                                                :oauth_consumer_key "<yourkey>"
+                                                :oauth_signature_method "HMAC-SHA1"
+                                                :oauth_timestamp "<timestamp>"
+                                                :oauth_nonce "<nonce>"
+                                                :oauth_version "1.0"}} ))))
